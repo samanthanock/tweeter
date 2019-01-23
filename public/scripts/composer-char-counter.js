a2cf1fd3f87016dc8b743f69d79ad80019981c54
samanthamarienock <sammarienock@gmail.com>
2019-01-23 00:03:51 -0800

    creates decrementing counter that changes colour

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,20 +2,19 @@
 $(function() {
   // need to implement a character counter that counts down the characters while the client types
   // need to access the textbox in the new tweet container
-  // $('.text').on('input', function() {
-  //   // need to say that while x is happening in the text box x is happening to char count
-  //   // subrtract character limit from tweet count... reminaingCharacters = character limint - value inputed into form (.val())
-  //   let charactersRemaining = 140 - $('.text').val().length;
-  //   //this counts it down in the counter
-  //   $('.counter').text(charactersRemaining);
-  //   //this should work?
-  //   if (charactersRemaining < 140) {
-  //     $('.counter').css('color', 'red');
-  //     // } else {
-  //     //   $('span.counter').css('color', 'black');
-  //   }
-  // });
+  $('#newTweetText').on('input', function() {
+    //   // need to say that while x is happening in the text box x is happening to char count
+    //   // subrtract character limit from tweet count... reminaingCharacters = character limint - value inputed into form (.val())
+    let charactersRemaining = 140 - $(this).val().length;
+    //   //this counts it down in the counter
+    $('.counter').text(charactersRemaining);
+    //   //this should work?
+    if (charactersRemaining > 0) {
+      $('.counter').css('color', 'black');
+    } else {
+      $('.counter').css('color', 'red');
+    }
+  });
   // write logic that says if keystrokes happen <250 times return error
   //also produce red line under text when <250 characters occurs
-  $('h2').css('color', 'pink');
 });
